2d0220c4a123f5efdb211f5a63997b900b432679
Kanne Samrudhi <134263151+Kanne-Samrudhi@users.noreply.github.com>
2024-12-07 18:54:45 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter n value");
+		int n=sc.nextInt();
+		System.out.println("enter m value");
+		int m=sc.nextInt();
+		//for(int i=1;i<=m;i++)
+			//System.out.println(n+"*"+i+"="+n*i);
+		for(int i=1;i<=1;i++) {
+			for(int j=1;j<=m;j++) {
+				System.out.println(n+"*"+j+"="+n*j);
+			}
+		}
+
+	}
+}
